f16becc1f35dffd79556a419083a6dee7a5f3eb3
pelgraine <140762863+pelgraine@users.noreply.github.com>
2026-04-24 22:15:43 +1000

    update firmware build date and version, increase ble tx power for meshpocket

--- examples/companion_radio/MyMesh.h
+++ examples/companion_radio/MyMesh.h
@@ -8,11 +8,11 @@
 #define FIRMWARE_VER_CODE 10
 
 #ifndef FIRMWARE_BUILD_DATE
-#define FIRMWARE_BUILD_DATE "17 Apr 2026"
+#define FIRMWARE_BUILD_DATE "24 Apr 2026"
 #endif
 
 #ifndef FIRMWARE_VERSION
-#define FIRMWARE_VERSION "v1.14.1"
+#define FIRMWARE_VERSION "v1.14.1M"
 #endif
 
 #if defined(NRF52_PLATFORM) || defined(STM32_PLATFORM)
