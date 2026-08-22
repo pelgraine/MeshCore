5f37e9cffcc5d7c2518ec3211c649219d5d4e943
pelgraine <140762863+pelgraine@users.noreply.github.com>
2026-08-22 14:55:19 +1000

    update correct build date

--- examples/simple_repeater/MyMesh.h
+++ examples/simple_repeater/MyMesh.h
@@ -67,7 +67,7 @@ struct NeighbourInfo {
 };
 
 #ifndef FIRMWARE_BUILD_DATE
-  #define FIRMWARE_BUILD_DATE   "14 Aug 2026"
+  #define FIRMWARE_BUILD_DATE   "22 Aug 2026"
 #endif
 
 #ifndef FIRMWARE_VERSION
